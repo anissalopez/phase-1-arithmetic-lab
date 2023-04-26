6e595094e04ca8676539a953884ca9b65706eb48
anissalopez <anissa965@gmail.com>
2023-04-25 21:51:58 -0400

    ath is hard

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+let multiply = num1 * num2;
+
+let random = Math.round(Math.random() + 1)
+
+
+let num3 = 46;
+let num4 = 6;
+
+let mod = num3 % num4;
+
+let max = Math.max(1,3,5,20)
